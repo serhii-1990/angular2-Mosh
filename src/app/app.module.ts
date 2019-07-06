c71342f215c73dc8320b7263e45d4d3bc11b0f4d
Serhii Tymchak <2freemangordon3@gmail.com>
2019-07-06 15:01:03 +0300

    Angualr fundamentals end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
+import { Courses } from "./courses.component";
+
+import { CoursesService } from "./courses.servise";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, Courses],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/courses.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { CoursesService } from "./courses.servise";
+
+@Component({
+  selector: "courses",
+  template: `
+    <h1>{{ mainHeader }}</h1>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `
+})
+export class Courses {
+  mainHeader = "Angular courses";
+  courses;
+
+  constructor(servise: CoursesService) {
+    this.courses = servise.getCourses();
+  }
+  getTitle() {
+    return this.mainHeader;
+  }
+}
