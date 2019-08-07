2f20e822cd2dba66177592d63e5c641f57db9ab5
Serhii Tymchak <2freemangordon3@gmail.com>
2019-08-07 07:40:59 +0300

    Example of builtIn pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,12 +4,19 @@ import { CoursesService } from "./courses.service";
 @Component({
   selector: "courses",
   template: `
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+    {{ course.title | uppercase }} <br />
+    {{ course.students | number }} <br />
+    {{ course.rating | number: "1.1-1" }} <br />
+    {{ course.price }} <br />
+    {{ course.relaseData }} <br />
   `
 })
 export class Courses {
-  email;
-  onKeyUp() {
-    console.log(this.email);
-  }
+  course = {
+    title: "The Complite Angular Course",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    relaseData: new Date(2016, 3, 1)
+  };
 }
