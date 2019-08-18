e55d550d8ba883ca82ea4cf6a41646bf242c7521
Serhii Tymchak <2freemangordon3@gmail.com>
2019-08-18 22:37:54 +0300

    approache how to handle complex obj with save traversal operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,10 @@ import { FavoriteChangedEventArgs } from "./favorite/favorite.component";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  courses;
-
-  getCourses() {
-    this.courses = [
-      { id: 1, name: "Course1" },
-      { id: 2, name: "Course2" },
-      { id: 3, name: "Course3" }
-    ];
-  }
-
-  trackCourse(index, course) {
-    course ? course.id : undefined;
-  }
+  tack = {
+    name: "task1",
+    assignee: {
+      name: "Lorem"
+    }
+  };
 }
